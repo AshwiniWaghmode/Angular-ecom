c6617aeb8638a83350caba465e59ab455485f334
ashwini <ashwiniwagh690@gmail.com>
2023-08-05 02:00:22 +0530

    done

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,9 +3,6 @@ import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route) => {
 
-  if (localStorage.getItem('seller')) {
-    return true;
-
+  
   return true;
-}
-}
+};
